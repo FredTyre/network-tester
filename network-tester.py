a0acee7b7b7557a4bd3b43653fe776803b54c100
FredTyre <ftyre76@gmail.com>
2024-07-17 21:50:22 -0500

    changed to ifaddr

--- network-tester.py
+++ network-tester.py
@@ -1,7 +1,7 @@
 import dns.resolver
 import socket
 import re
-import netifaces-plus
+import ifaddr
 
 from urllib.request import urlopen
 
@@ -47,7 +47,7 @@ def local_ip_address():
     return ip_address
 
 def get_network_interfaces():
-    return netifaces.interfaces()
+    return ifaddr.get_adapters()
 
 def comma_delimited_string_to_list(string_to_convert):
     this_list = string_to_convert.split(",")
@@ -59,37 +59,9 @@ def comma_delimited_string_to_list(string_to_convert):
 def full_network_info():
     network_interfaces = {}
     for net_interface in get_network_interfaces():
-        if net_interface == 'lo':
-            continue
-        network_interfaces[net_interface] = {}
-        print(f"Network interface: {net_interface}")
-        interface_list = comma_delimited_string_to_list(str(netifaces.ifaddresses(net_interface)))
-        for interface in interface_list:
-            string_to_parse = interface.strip(" ").strip("{").strip("}")
-            begin_bracket = string_to_parse.find("[")
-            if begin_bracket != -1:
-                end_bracket = string_to_parse.find("]", begin_bracket)
-            else:
-                end_bracket = string_to_parse.find("]")
-            if begin_bracket == -1:
-                begin_bracket = 0
-            if end_bracket == -1:
-                end_bracket = len(string_to_parse) - 1
-            string_to_parse = string_to_parse[begin_bracket:end_bracket].strip("[").strip("]")
-            string_to_parse = interface.strip(" ").strip("{").strip("}")
-            addresses = comma_delimited_string_to_list(string_to_parse)
-            for address in addresses:
-                parts = address.split(":")
-                if len(parts) > 1:
-                    address_name = parts[0].strip("'")
-                if len(parts) > 2:
-                    actual_address = parts[1].strip("'")
-                    network_interfaces[net_interface][address_name] = actual_address
-        gateways = netifaces.gateways()
-        for gateway in gateways:
-            print(gateway)
-
-    print(network_interfaces)
+        print(f"Network interface: {net_interface.nice_name}")
+        for ip_address in net_interface.ips:
+            print("   %s/%s" % (ip_address.ip, ip_address.network_prefix))
 
 def main():
     # gateway
